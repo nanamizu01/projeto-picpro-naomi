4e15187127d3b86c0b23266a90a894b2411f7383
naomi <89711375+nanamizu01@users.noreply.github.com>
2022-03-08 21:00:00 -0300

    att javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,24 @@
 const listaOpcoes=document.querySelector(".secaoDetalhesConta_opcoesTransacoes")
 
-listaOpcoes.addEventListener("click", identificarOpcoes)
+listaOpcoes.addEventListener("click", identificarOpcoesTransacao)
 
-function identificarOpcoes(event){
+function identificarOpcoesTransacao(event){
     const elemento=event.target
 
     if(elemento.tagName == "LI"){
         const id=elemento.id
-        const opcaoSelecionada=document.querySelector('div[data-id="${id}"]')
-        console.log(opcaoSelecionada)
+        const opcaoSelecionada=document.querySelector(`div[data-id="${id}"]`)
+        
+        removeClasseMostrar()
+
+        opcaoSelecionada.classList.add("mostrar")
+    }
+}
+
+function removeClasseMostrar(){
+    const listaDivs=document.querySelectorAll(".secaoTransacao .container div")
+    
+    for(let i=0;i<listaDivs.length;i++){
+        listaDivs[i].classList.remove("mostrar")
     }
 }
